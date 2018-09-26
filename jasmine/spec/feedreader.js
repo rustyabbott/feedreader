2ae1322de7ae9b20b7c9dae6ec710f2df2489182
Rusty Abbott <rusty@hutchinson-island.com>
2018-09-25 20:31:04 -0400

    Write test checking URLs for each feed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,6 +32,13 @@ $(function() {
          * and that the URL is not empty.
          */
 
+         it('has URL', function() {
+           allFeeds.forEach(function(feed) {
+             expect(feed.url).toBeDefined();
+             expect(feed.url.length).not.toBe(0);
+           })
+         })
+
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
